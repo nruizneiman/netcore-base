32df7f024f9985551aee1f7c7f7f7f005d7a56a5
Nicolás Ruiz Neiman <nicolaseas@hotmail.com>
2020-11-01 00:00:54 -0300

    Using clean up

--- SomeProject/Infrastructure/ServiceCollectionExtension.cs
+++ SomeProject/Infrastructure/ServiceCollectionExtension.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace Infrastructure
 {
