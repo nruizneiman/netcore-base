473127f469c947b584297f445b3dbb067c32ff3a
Nicolás Ruiz Neiman <nicolasruizneiman@gmail.com>
2020-11-01 21:53:51 -0300

    Basic CSV file builder implementation

---
+++ SomeProject/Core/IFileBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Core
+{
+    public interface IFileBuilder<T>
+    {
+        byte[] ToCsv(IEnumerable<T> records);
+        byte[] ToPdf(IEnumerable<T> records);
+        byte[] ToPdf(string html);
+    }
+}
---
+++ SomeProject/Infrastructure/FileBuilder.cs
@@ -0,0 +1,40 @@
+﻿using Core;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Infrastructure
+{
+    public class FileBuilder<T> : IFileBuilder<T>
+    {
+        #region CSV
+        public byte[] ToCsv(IEnumerable<T> records)
+        {
+            using var memoryStream = new MemoryStream();
+            using (var streamWriter = new StreamWriter(memoryStream))
+            {
+                using var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture);
+
+                csvWriter.Configuration.AutoMap<T>();
+                csvWriter.WriteRecords(records);
+            }
+
+            return memoryStream.ToArray();
+        }
+        #endregion
+
+        #region PDF
+        public byte[] ToPdf(IEnumerable<T> records)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] ToPdf(string html)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
